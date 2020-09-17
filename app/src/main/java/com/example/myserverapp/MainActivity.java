c27bcc9b43a7dcedc2df630c61e7eebaad06e78c
rimjhimsudo <rimjhimvaish@gmail.com>
2020-09-17 20:39:06 +0530

    first commit

---
+++ app/src/main/java/com/example/myserverapp/MainActivity.java
@@ -0,0 +1,262 @@
+package com.example.myserverapp;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ServerSocket serverSocket;
+    private Socket tempClientSocket;
+    Thread serverThread = null;
+    public static final int SERVER_PORT = 3003;
+    private LinearLayout msgList;
+    private Handler handler;
+    private int greenColor;
+    private EditText edMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Server");
+        greenColor = ContextCompat.getColor(this, R.color.colorAccent);
+        handler = new Handler();
+        msgList = findViewById(R.id.msgList);
+        edMessage = findViewById(R.id.edMessage);
+    }
+
+    public TextView textView(String message, int color) {
+        if (null == message || message.trim().isEmpty()) {
+            message = "<Empty Message>";
+        }
+        TextView tv = new TextView(this);
+        tv.setTextColor(color);
+        tv.setText(message + " [" + getTime() + "]");
+        tv.setTextSize(20);
+        tv.setPadding(0, 5, 0, 0);
+        return tv;
+    }
+
+    public void showMessage(final String message, final int color) {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                msgList.addView(textView(message, color));
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.start_server) {
+            msgList.removeAllViews();
+            showMessage("Server Started.", Color.BLACK);
+            this.serverThread = new Thread(new ServerThread());
+            this.serverThread.start();
+            return;
+        }
+        if (view.getId() == R.id.send_data) {
+            String msg = edMessage.getText().toString().trim();
+            showMessage("Server : " + msg, Color.BLUE);
+            //sendMessage(msg);
+        }
+    }
+
+    private void sendMessage(final String message) {
+        try {
+            Log.d("tempClientSocket",":  not null");
+            if (null != tempClientSocket) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PrintWriter out = null;
+                        try {
+                            out = new PrintWriter(new BufferedWriter(
+                                    new OutputStreamWriter(tempClientSocket.getOutputStream())),
+                                    true);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        out.println(message);
+                    }
+                }).start();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ServerThread implements Runnable {
+
+        public void run() {
+            Socket socket;
+            try {
+                serverSocket = new ServerSocket(SERVER_PORT);
+                //findViewById(R.id.start_server).setVisibility(View.GONE);
+            } catch (IOException e) {
+                e.printStackTrace();
+                showMessage("Error Starting Server : " + e.getMessage(), Color.RED);
+            }
+            if (null != serverSocket) {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        socket = serverSocket.accept();
+                        Log.d("SOCKET value  : ",socket.toString());
+                        CommunicationThread commThread = new CommunicationThread(socket);
+                        new Thread(commThread).start();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        showMessage("Error Communicating to Client :" + e.getMessage(), Color.RED);
+                    }
+                }
+            }
+        }
+    }
+
+    class CommunicationThread implements Runnable {
+
+        private Socket clientSocket;
+
+        private BufferedReader input;
+
+        public CommunicationThread(Socket clientSocket) {
+            this.clientSocket = clientSocket;
+            tempClientSocket = clientSocket;
+            try {
+                this.input = new BufferedReader(new InputStreamReader(tempClientSocket.getInputStream()));
+               // this.input = new BufferedReader(new InputStreamReader(this.clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+                showMessage("Error Connecting to Client!!", Color.RED);
+            }
+            showMessage("Connected to Client!!", greenColor);
+        }
+
+        public void run() {
+            while (!Thread.currentThread().isInterrupted()) {
+                try {
+                    String read = input.readLine();
+                    if (null == read || "Disconnect".contentEquals(read)) {
+                        Thread.interrupted();
+                        read = "Client Disconnected";
+                        showMessage("Client : " + read, greenColor);
+                        Log.d("SERVERSIDE","CLient msg on server recived"+read);
+                        break;
+                    }
+                    showMessage("Client : " + read, greenColor);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+
+    }
+
+    String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (null != serverThread) {
+            sendMessage("Disconnect");
+            serverThread.interrupt();
+            serverThread = null;
+        }
+    }
+}
+
+/*
+File file = new File(
+     Environment.getExternalStorageDirectory(),
+     "android-er_sketch_1000.png");
+
+   byte[] bytes = new byte[(int) file.length()];
+   BufferedInputStream bis;
+   try {
+    bis = new BufferedInputStream(new FileInputStream(file));
+    bis.read(bytes, 0, bytes.length);
+
+    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+    oos.writeObject(bytes);
+    oos.flush();
+ */
+
+/*
+public class FileTxThread extends Thread {
+  Socket socket;
+
+  FileTxThread(Socket socket){
+   this.socket= socket;
+  }
+
+  @Override
+  public void run() {
+   File file = new File(
+     Environment.getExternalStorageDirectory(),
+     "android-er_sketch_1000.png");
+
+   byte[] bytes = new byte[(int) file.length()];
+   BufferedInputStream bis;
+   try {
+    bis = new BufferedInputStream(new FileInputStream(file));
+    bis.read(bytes, 0, bytes.length);
+
+    ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+    oos.writeObject(bytes);
+    oos.flush();
+
+    socket.close();
+
+    final String sentMsg = "File sent to: " + socket.getInetAddress();
+    MainActivity.this.runOnUiThread(new Runnable() {
+
+     @Override
+     public void run() {
+      Toast.makeText(MainActivity.this,
+        sentMsg,
+        Toast.LENGTH_LONG).show();
+     }});
+
+   } catch (FileNotFoundException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+   } catch (IOException e) {
+    // TODO Auto-generated catch block
+    e.printStackTrace();
+   } finally {
+    try {
+     socket.close();
+    } catch (IOException e) {
+     // TODO Auto-generated catch block
+     e.printStackTrace();
+    }
+   }
+
+  }
+ }
+ */
